85f50c1400dfb89ab93892f2e4b73dfed1e7cbeb
mlgarchery <martial.garchery@gmail.com>
2024-04-27 20:40:40 +0200

    fix header

--- ui/src/components/common/Header/Header.tsx
+++ ui/src/components/common/Header/Header.tsx
@@ -3,13 +3,20 @@ import { Stack } from "@/components/common/Stack";
 import { Text } from "@/components/common/Text";
 import { PropsWithStyles } from "@/types/styles";
 import { theme } from "@/styles/theme";
+import { HostInput } from "./HostInput";
+import { ModelSelect } from "./ModelSelect";
 
 export interface HeaderProps extends PropsWithStyles {}
 
 export const Header = () => {
+  const handleHostnameChange = () => console.log("hostname changed");
+  const handleModelChange = () => console.log("model changed!");
+
   return (
     <HeaderRoot>
       <Text type="M">You Shall Not Pass (PB#5)</Text>
+      <HostInput onChange={handleHostnameChange} />
+      <ModelSelect onChange={handleModelChange} />
     </HeaderRoot>
   );
 };
--- ui/src/components/pages/main/Main.tsx
+++ ui/src/components/pages/main/Main.tsx
@@ -4,8 +4,6 @@ import { Stack } from "@/components/common/Stack";
 import { Text } from "@/components/common/Text";
 import { theme } from "@/styles/theme";
 import reactLogo from "@/assets/images/react.svg";
-import { HostInput } from "@/components/common/HostInput";
-import { ModelSelect } from "@/components/common/ModelSelect";
 import { ConversationDisplay } from "@/components/common/ConversationDisplay";
 import { MessageInput } from "@/components/common/MessageInput";
 import { text } from "stream/consumers";
@@ -13,8 +11,6 @@ import { text } from "stream/consumers";
 export interface MainProps extends PropsWithStyles {}
 
 export const Main = () => {
-  const handleHostnameChange = () => console.log("hostname changed");
-  const handleModelChange = () => console.log("model changed!");
   const handleMessageSubmit = () => console.log("submitted messages");
 
   const messages = [
@@ -23,8 +19,6 @@ export const Main = () => {
   ];
   return (
     <MainRoot>
-      <HostInput onChange={handleHostnameChange} />
-      <ModelSelect onChange={handleModelChange} />
       <ConversationDisplay messages={messages} />
       <MessageInput onSubmit={handleMessageSubmit} />
     </MainRoot>
