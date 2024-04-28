b050d063ab203559eae913899f0cffe82c5fbfdb
mlgarchery <martial.garchery@gmail.com>
2024-04-28 17:02:06 +0200

    css fix and adding gandalf

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -8,6 +8,7 @@ import { LevelSelect } from "./LevelSelect";
 import { getClasses } from "@/utils/class";
 import { useAppContext } from "@/contexts/app.context";
 
+
 export interface HeaderProps extends PropsWithStyles {}
 
 export const Header = () => {
@@ -15,7 +16,8 @@ export const Header = () => {
 
   return (
     <HeaderRoot>
-      <Text type="M">You Shall Not Pass (PB#5)</Text>
+      <img alt="gandalf logo" className={getClasses("sub-cont")} src="/src/assets/images/gandalf.jpg"/>
+      <Text type="M">Thou Shall Not Pass!</Text>
       <div className={getClasses("sub-cont")}>
         <HostInput onChange={handleHostnameChange} />
         <LevelSelect />
@@ -28,14 +30,21 @@ export const Header = () => {
 const HeaderRoot = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items:center;
   span {
     display: flex;
     font-size: 35px;
     color: #3d3dff;
     margin: 25px 0px;
   }
+  img{
+    border-radius: 50%;
+    &:hover{
+      animation: 2s bounce;
   .sub-cont {
     display: flex;
     flex-direction: column;
   }
 `;
+
+
